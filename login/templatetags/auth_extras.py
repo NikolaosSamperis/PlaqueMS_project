c1dd89cf7d0f1e2b9d50b035cb1cb6a4ce76ff5b
NikosSab <K24105804@kcl.ac.uk>
2025-12-25 00:07:52 +0200

    deleted:    login/__init__.py 	deleted:    login/admin.py 	deleted:    login/apps.py 	deleted:    login/auth_views.py 	deleted:    login/calc_pred_views.py 	deleted:    login/cyviews.py 	deleted:    login/forms.py 	deleted:    login/home_views.py 	deleted:    login/insert_views.py 	deleted:    login/management/__init__.py 	deleted:    login/management/commands/__init__.py 	deleted:    login/management/commands/insert_uva.py 	deleted:    login/management/commands/insert_vienna.py 	deleted:    login/migrations/0001_initial.py 	deleted:    login/migrations/__init__.py 	deleted:    login/models.py 	deleted:    login/networkTree.py 	deleted:    login/pathTree.py 	deleted:    login/plaquery_views.py 	deleted:    login/plot_views.py 	deleted:    login/protein_views.py 	deleted:    login/syntax_score_views.py 	deleted:    login/templates/Home.html 	deleted:    login/templates/calc_pred.html 	deleted:    login/templates/login/admin_dashboard.html 	deleted:    login/templates/login/login.html 	deleted:    login/templates/login/register.html 	deleted:    login/templates/network.html 	deleted:    login/templates/plaquery.html 	deleted:    login/templates/plot.html 	deleted:    login/templates/plot_result_list.html 	deleted:    login/templates/protein.html 	deleted:    login/templates/syntax_pred.html 	deleted:    login/templatetags/__init__.py 	deleted:    login/templatetags/auth_extras.py 	deleted:    login/templatetags/navigation.py 	deleted:    login/tests.py 	deleted:    login/validators.py

--- login/templatetags/auth_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from django.template.defaultfilters import stringfilter
-
-register = template.Library()
-
-@register.filter(name='is_authenticated')
-def is_authenticated(user):
-    return user.is_authenticated 
